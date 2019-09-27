5a94f766cfb6aee97ba00fec9a9efb797bcd9631
12124893 <41608267+12124893@users.noreply.github.com>
2019-09-27 16:31:35 +1000

    Update Brobot.m

--- Brobot.m
+++ Brobot.m
@@ -8,10 +8,10 @@ L2 = Link('d',0,'a',0.14,'alpha',0,'qlim',[deg2rad(0) deg2rad(85)]);
 L3 = Link('d',0,'a',0.16,'alpha',0,'qlim',[deg2rad(10) deg2rad(95)]);
 L4 = Link('d',0,'a',0.05,'alpha',-pi/2,'qlim',[deg2rad(90) deg2rad(-90)]); 
 
-base = transl(0,0,0.25);
-Brobot = SerialLink([L1 L2 L3 L4],'name','Brobot','base',base);                     
-q = [0,pi/4,-pi/2,pi/4];
+base = transl(0,0,0.25); %Puts the base of the robot at this specific coordinate. 
+Brobot = SerialLink([L1 L2 L3 L4],'name','Brobot','base',base); %setting the parameters of the robot                  
+q = [0,pi/4,-pi/2,pi/4]; %initial positioning of the joints
 scale = 0.5;
-workspace = [-1 1 -1 1 -0.05 1];
-Brobot.plot(q,'workspace',workspace,'scale',scale);
+workspace = [-1 1 -1 1 -0.05 1]; %boundary of the working environment
+Brobot.plot(q,'workspace',workspace,'scale',scale); %plotting the robot
 hold on;
