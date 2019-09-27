5e3fc338936d3c508082a0f8210b56f698858f13
SanjiHam <46935097+SanjiHam@users.noreply.github.com>
2019-09-27 16:03:52 +1000

    Add files via upload

---
+++ Brobot.m
@@ -0,0 +1,17 @@
+function Brobot()
+clc
+clc
+
+%% Plot Brobot
+L1 = Link('d',0.08,'a',0,'alpha',pi/2,'qlim',[deg2rad(-90) deg2rad(90)]);
+L2 = Link('d',0,'a',0.14,'alpha',0,'qlim',[deg2rad(0) deg2rad(85)]);
+L3 = Link('d',0,'a',0.16,'alpha',0,'qlim',[deg2rad(10) deg2rad(95)]);
+L4 = Link('d',0,'a',0.05,'alpha',-pi/2,'qlim',[deg2rad(90) deg2rad(-90)]); 
+
+base = transl(0,0,0.25);
+Brobot = SerialLink([L1 L2 L3 L4],'name','Brobot','base',base);                     
+q = [0,pi/4,-pi/2,pi/4];
+scale = 0.5;
+workspace = [-1 1 -1 1 -0.05 1];
+Brobot.plot(q,'workspace',workspace,'scale',scale);
+hold on;
