10f0d89c30c194c8b5e5d4038cf205ff054c57b8
SanjidHam <46935097+SanjiHam@users.noreply.github.com>
2019-10-08 21:31:39 +1100

    camera and image detection

--- KinectCamera.m
+++ KinectCamera.m
@@ -1,17 +1,81 @@
-%% Setup Kinect v1.6 Runtime
-targetinstaller
+%% View Image
+clear all;
+colorDevice = imaq.VideoDevice('kinect',1);
+depthDevice = imaq.VideoDevice('kinect',2);
+  
+colorDevice();
+depthDevice();
+  
+colorImage = colorDevice();
+depthImage = depthDevice();
+  
+ptCloud = pcfromkinect(depthDevice, depthImage, colorImage);
 
-%% Kinect adaptor and devices
-info = imaqhwinfo('kinect')
+release(colorDevice);
+release(depthDevice);
+idisp(colorImage);
 
-%% Create videoinput object for colour stream
-info.DeviceInfo(1)
-colorVid = videoinput('kinect',1,'RGB_640x360');
-preview(colorVid);
 
-%% Videoinput object for depth stream
-info.DeviceInfo(2)
-depthVid = videoinput ('kinect',2,'Depth_640x480');
+%% Circle detection
+% I = colorImage;
+rgb = imread('coloredChips.png');
+% rgb = imread('c.jpg');
+idisp(rgb)
+% rgb = imcrop(I,[1220 500 500]); %[initial u ,initial v,Length, width] %Image crop
 
-depthImage = getsnapshot(depthVid);
-imshow(depthImage, [0 4000])
+gray_image = rgb2gray(rgb);
+imshow(gray_image)
+
+[~,~] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark');
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+    'Sensitivity',0.9);
+imshow(rgb)
+h = viscircles(centers,radii);
+
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+    'Sensitivity',0.92);
+
+length(centers)
+delete(h)  % Delete previously drawn circles
+h = viscircles(centers,radii);
+
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+          'Sensitivity',0.92,'Method','twostage');
+
+delete(h)
+h = viscircles(centers,radii);
+
+[centers,radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark', ...
+          'Sensitivity',0.95);
+
+delete(h)
+viscircles(centers,radii);
+% imshow(gray_image)
+% 
+% [centersBright,radiiBright] = imfindcircles(rgb,[20 25], ...
+%     'ObjectPolarity','bright','Sensitivity',0.92);
+% 
+% imshow(rgb)
+% 
+% hBright = viscircles(centersBright, radiiBright,'Color','b');
+% 
+% [centersBright,radiiBright,metricBright] = imfindcircles(rgb,[20 25], ...
+%     'ObjectPolarity','bright','Sensitivity',0.92,'EdgeThreshold',0.1);
+% 
+% delete(hBright)
+% hBright = viscircles(centersBright, radiiBright,'Color','b');
+% 
+% h = viscircles(centers,radii);
+
+% %% Square Recognition
+% 
+% Ibw = ~im2bw(I,graythresh(I));
+% Ifill = imfill(Ibw,'holes');
+% Iarea = bwareaopen(Ifill,100);
+% Ifinal = bwlabel(Iarea);
+% stat = regionprops(Ifinal,'boundingbox');
+% imshow(I); hold on;
+% for cnt = 1 : numel(stat)
+%     bb = stat(cnt).BoundingBox;
+%     rectangle('position',bb,'edgecolor','r','linewidth',2);
+% end
