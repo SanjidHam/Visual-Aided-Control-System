ee2d3c632464e3fe49936c3b8f44ad733aa2ea9d
SanjidHam <46935097+SanjiHam@users.noreply.github.com>
2019-10-13 19:31:45 +1100

    sim

--- Brobot/Brobot_simulation.m
+++ Brobot/Brobot_simulation.m
@@ -63,6 +63,19 @@ for xOffset = [-1.19, -1.19]
     end
 end
 
+%Fire Extinguisher
+[f,v,topH] = plyread('fireEX.ply','tri');
+vertexColours = [topH.vertex.red, topH.vertex.green, topH.vertex.blue]/255;
+hold on;
+for xOffset = [-1, -1]
+    for zOffset =  [-0.6,-0.6]  
+        for yOffset = [-1.0,-1.0]
+            trisurf(f,v(:,1)+ xOffset,v(:,2)+ yOffset,v(:,3) + zOffset...
+            ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+        end
+    end
+end
+
 hold on;
 
 %Caution Poster
