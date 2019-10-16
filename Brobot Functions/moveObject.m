59fbc19a3afa9bee066799832469016c5a872455
SanjidHam <46935097+SanjiHam@users.noreply.github.com>
2019-10-17 00:06:24 +1100

    object placement

--- Brobot Functions/moveObject.m
+++ Brobot Functions/moveObject.m
@@ -1,17 +1,7 @@
-function move(self, pose)
-            forwardTR = makehgtform('translate',pose);
-            self.objectPose = self.objectPose*forwardTR; %update the location
-            updatedPoints = [self.objectPose * [self.objectVerts,ones(self.objectVertexCount,1)]']';  %update the location
-            self.objectMesh_h.Vertices = updatedPoints(:,1:3);
+function moveObject(Obj,pose)
+forwardTR = makehgtform('translate',pose);
+Obj.objectPose = Obj.objectPose*forwardTR; %update the location
+updatedPoints = [Obj.objectPose * [vNew,ones(Obj.objectVertexCount,1)]']';  %update the location
+Obj.objectMesh.Vertices = updatedPoints(:,1:3);
 end
-objectVertexCount = size(v,3);%%%%get
-midPoint = sum(v)/objectVertexCount;
-objectPose = eye(4); % create the matrix 4x4 for partpose
-objectVerts = v - repmat(midPoint,objectVertexCount,1);%%%%get
-            
-        
-%% Function to move the object
-forwardTR = makehgtform('translate',[0.1,0.2,0.3]);
-objectPose = objectPose*forwardTR; %update the location
-updatedPoints = [objectPose * [objectVerts,ones(objectVertexCount,1)]']';  %update the location
-objectMesh_h.Vertices = updatedPoints(:,1:3);
+
--- Brobot/Brobot_simulation.m
+++ Brobot/Brobot_simulation.m
@@ -47,41 +47,23 @@ hold on;
 
 %% Demo Accessories
 %Small Circle
-[f,v,data] = plyread('circleSmall.ply','tri');
-objectVertexCount = size(v,1);%%%%get
-midPoint = sum(v)/objectVertexCount;
-objectPose = eye(4); % create the matrix 4x4 for partpose
-objectVerts = v - repmat(midPoint,objectVertexCount,1);%%%%get
-vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue]/255;
-hold on;
-for zOffset = [0.01,0.01]
-    for xOffset = [0,0]   
-        for yOffset = [0.2,0.2]
-            trisurf(f,v(:,1)+ xOffset,v(:,2)+ yOffset,v(:,3) + zOffset...
-            ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-        end
-    end
-end
-hold on;               
-%Function to move the object
-forwardTR = makehgtform('translate',[0.2;0.2;0.3]);
-objectPose = objectPose*forwardTR; %update the location
-updatedPoints = [objectPose * [objectVerts,ones(objectVertexCount,1)]']';  %update the location
-objectMesh_h.Vertices = updatedPoints(:,1:3);
+placeObject('circleSmall.ply');  
+
+moveObject(Obj,[0.1,0.2,0.3]);
 
 %% Movement
 % s = 20;
-% T1 = transl(-0.25,0.18,0.15);
+% T1 = transl(0,0.18,0.1);
 % q1 = Brobot.ikcon(T1);
 % qMatrix = jtraj(q,q1,s);
 % Brobot.animate(qMatrix);
 % 
-% T2 = transl(-0.25,0.18,0.05);
+% T2 = transl(0,0.18,0.05);
 % q2 = Brobot.ikcon(T2);
 % qMatrix = jtraj(q1,q2,s);
 % Brobot.animate(qMatrix);
 % 
-% T3 = transl(-0.25,0.18,0.15);
+% T3 = transl(0,0.18,0.15);
 % q3 = Brobot.ikcon(T3);
 % qMatrix = jtraj(q2,q3,s);
 % Brobot.animate(qMatrix);
