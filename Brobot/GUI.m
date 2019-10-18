f31c34937c72df0542f809a2153e53ba52fb537d
SanjidHam <46935097+SanjiHam@users.noreply.github.com>
2019-10-18 15:31:04 +1100

    GUI

--- Brobot/GUI.m
+++ Brobot/GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 18-Oct-2019 04:45:13
+% Last Modified by GUIDE v2.5 18-Oct-2019 14:33:18
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -628,3 +628,163 @@ frwQ = Brobot.fkine([J_1 J_2 J_3 J_4 J_5]);
 % handles.Pos_X.String = num2str(floor(frwQ(1,4)));
 % handles.Pos_Y.String = num2str(floor(frwQ(2,4)));
 % handles.Pos_Z.String = num2str(floor(frwQ(3,4)));
+
+
+% --- Executes on button press in cdButton.
+function cdButton_Callback(hObject, eventdata, handles)
+% hObject    handle to cdButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+disp ('Collision Detection Simulation Started..');
+
+PX = str2double(handles.Pos_X.String);
+PY = str2double(handles.Pos_Y.String);
+PZ = str2double(handles.Pos_Z.String);
+
+Brobot = plotBrobot;
+
+%% intersect
+plotOptions.plotFaces = true;
+[vertex,faces,faceNormals] = RectangularPrism([0.3,-0.3,-0.03073], [-0.28,0.3,0.4493],plotOptions);
+hold on
+
+% % 2.4: Get the transform of every joint (i.e. start and end of every link)
+% tr = zeros(4,4,Brobot.n+1);
+% tr(:,:,1) = Brobot.base;
+% L = Brobot.links;
+% for i = 1 : Brobot.n
+%     tr(:,:,i+1) = tr(:,:,i) * trotz(q(i)+L(i).offset) * transl(0,0,L(i).d) * transl(L(i).a,0,0) * trotx(L(i).alpha);
+% end
+% 
+% % 2.5: Go through each link and also each triangle face
+% for i = 1 : size(tr,3)-1    
+%     for faceIndex = 1:size(faces,1)
+%         vertOnPlane = vertex(faces(faceIndex,1)',:);
+%         [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+%         if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+%             plot3(intersectP(1),intersectP(2),intersectP(3),'r*');
+%             display('Collision Detected');
+%         end
+%     end    
+% end
+%%
+
+t1 = transl(0,-0.25,0.2);
+q1 = Brobot.ikcon(T);
+t2 = transl(PX,PY,PZ);
+q2 = Brobot.ikcon(invT);
+ 
+% moveqstart2 = jtraj(q1, q2 , steps);
+% robot.animate(moveqstart2)
+
+steps = 2;
+while ~isempty(find(1 < abs(diff(rad2deg(jtraj(q1,q2,steps)))),1))
+    steps = steps + 1;
+end
+qMatrix = jtraj(q1,q2,steps);
+
+% 2.7
+result = true(steps,1);
+for i = 1: steps
+    %%
+    result(i) = IsCollision(Brobot,qMatrix(i,:),faces,vertex,faceNormals,false);
+    Brobot.animate(qMatrix(i,:));
+end
+%%
+function result = IsIntersectionPointInsideTriangle(intersectP,triangleVerts)
+
+u = triangleVerts(2,:) - triangleVerts(1,:);
+v = triangleVerts(3,:) - triangleVerts(1,:);
+
+uu = dot(u,u);
+uv = dot(u,v);
+vv = dot(v,v);
+
+w = intersectP - triangleVerts(1,:);
+wu = dot(w,u);
+wv = dot(w,v);
+
+D = uv * uv - uu * vv;
+
+% Get and test parametric coords (s and t)
+s = (uv * wv - vv * wu) / D;
+if (s < 0.0 || s > 1.0)        % intersectP is outside Triangle
+    result = 0;
+    return;
+end
+
+t = (uv * wu - uu * wv) / D;
+if (t < 0.0 || (s + t) > 1.0)  % intersectP is outside Triangle
+    result = 0;
+    return;
+end
+
+result = 1;                      % intersectP is in Triangle
+end
+%% IsCollision
+% This is based upon the output of questions 2.5 and 2.6
+% Given a robot model (robot), and trajectory (i.e. joint state vector) (qMatrix)
+% and triangle obstacles in the environment (faces,vertex,faceNormals)
+function result = IsCollision(Brobot,qMatrix,faces,vertex,faceNormals,returnOnceFound)
+if nargin < 6
+    returnOnceFound = true;
+end
+result = false;
+
+for qIndex = 1:size(qMatrix,1)
+    % Get the transform of every joint (i.e. start and end of every link)
+    tr = GetLinkPoses(qMatrix(qIndex,:), Brobot);
+
+    % Go through each link and also each triangle face
+    for i = 1 : size(tr,3)-1    
+        for faceIndex = 1:size(faces,1)
+            vertOnPlane = vertex(faces(faceIndex,1)',:);
+            [intersectP,check] = LinePlaneIntersection(faceNormals(faceIndex,:),vertOnPlane,tr(1:3,4,i)',tr(1:3,4,i+1)'); 
+            if check == 1 && IsIntersectionPointInsideTriangle(intersectP,vertex(faces(faceIndex,:)',:))
+                plot3(intersectP(1),intersectP(2),intersectP(3),'r*');
+                display('Collision Detected');
+                %stop robot
+                result = true;
+                if returnOnceFound
+                    return
+                end
+            end
+        end    
+    end
+end
+end
+%% GetLinkPoses
+% q - robot joint angles
+% robot -  seriallink robot model
+% transforms - list of transforms
+function [ transforms ] = GetLinkPoses( q, Brobot)
+
+links = Brobot.links;
+transforms = zeros(4, 4, length(links) + 1);
+transforms(:,:,1) = Brobot.base;
+
+for i = 1:length(links)
+    L = links(1,i);
+    
+    current_transform = transforms(:,:, i);
+    
+    current_transform = current_transform * trotz(q(1,i) + L.offset) * ...
+    transl(0,0, L.d) * transl(L.a,0,0) * trotx(L.alpha);
+    transforms(:,:,i + 1) = current_transform;
+end
+end
+
+%% FineInterpolation
+% Use results from Q2.6 to keep calling jtraj until all step sizes are
+% smaller than a given max steps size
+function qMatrix = FineInterpolation(q1,q2,maxStepRadians)
+if nargin < 3
+    maxStepRadians = deg2rad(1);
+end
+    
+steps = 2;
+while ~isempty(find(maxStepRadians < abs(diff(jtraj(q1,q2,steps))),1))
+    steps = steps + 1;
+end
+qMatrix = jtraj(q1,q2,steps);
+end
