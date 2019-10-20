0a859177d31411e6abff6b81d4601471b8302d03
12124893 <41608267+12124893@users.noreply.github.com>
2019-10-20 21:32:17 +1100

    Update Brobot_run.m

--- Brobot/Brobot_run.m
+++ Brobot/Brobot_run.m
@@ -7,68 +7,32 @@ rosinit('http://10.42.0.1:11311')
 cartsvc_ = rossvcclient('/dobot_magician/PTP/set_cartesian_pos');
 cartmsg_ = rosmessage(cartsvc_);
 
-%%
-%Setting the Position for the end effector
-cartmsg_.TargetPoints=[0.142,0.185,0.054,0];
-cartsvc_.call(cartmsg_)
+%% camera 
+clear all
 
-cartmsg_.TargetPoints=[0.015,0.289,0.071,0];
-cartsvc_.call(cartmsg_)
+% taking a picture with camera and identifying the shape
+cam = webcam(2);
+f = snapshot(cam);
+imshow(f)
+rgb = imcrop(f, [138 144 160 600]);
+% figure
+ imshow (rgb)
 
-cartmsg_.TargetPoints=[0.015,0.289,-0.076,0];
-cartsvc_.call(cartmsg_)
+% identifying the square shape
+getSquare(rgb)
 
-%Turn ON Suction
-grippermsg_.IsEndEffectorEnabled=1;
-grippermsg_.EndEffectorState=1;
-grippersvc_.call(grippermsg_);
+% identifying the circle shape
+% circleDetection1;
+if getSquare(rgb) == 1
+    pickSquare % picking up the square shape from secondary tray
+    dropSquare % placing the sqaure shape on the primary shape
+    
+else 
+    pickCircle % picking up the circle shape from secondary tray
+    dropCircle % placing the circle shape on the primary shape
+end
 
-cartmsg_.TargetPoints=[0.015,0.289,0.059,0];
-cartsvc_.call(cartmsg_)
-
-cartmsg_.TargetPoints=[0.25,0.015,0.1,0];
-cartsvc_.call(cartmsg_)
-
-cartmsg_.TargetPoints=[0.243,0.027,-0.05,0];
-cartsvc_.call(cartmsg_)
-
-%Turn OFF Suction
-grippermsg_.IsEndEffectorEnabled=0;
-grippermsg_.EndEffectorState=0;
-grippersvc_.call(grippermsg_);
-
-cartmsg_.TargetPoints=[0.251,0.011,0.04,0];
-cartsvc_.call(cartmsg_)
-%%
-%End Efffector Variables Setup
-grippersvc_ = rossvcclient('/dobot_magician/end_effector/set_gripper');
-grippermsg_ = rosmessage(grippersvc_);
-
-%%
-%Turn ON Suction
-grippermsg_.IsEndEffectorEnabled=1;
-grippermsg_.EndEffectorState=1;
-grippersvc_.call(grippermsg_);
-
-%%
-%Turn OFF Suction
-grippermsg_.IsEndEffectorEnabled=0;
-grippermsg_.EndEffectorState=0;
-grippersvc_.call(grippermsg_);
-
-%%
-%Gripper Open
-grippermsg_.IsEndEffectorEnabled=1;
-grippermsg_.EndEffectorState=0;
-grippersvc_.call(grippermsg_);
-
-%%
-%Gripper Closed
-grippermsg_.IsEndEffectorEnabled=1;
-grippermsg_.EndEffectorState=1;
-grippersvc_.call(grippermsg_);
-
-%%
-%Moving End Effector
-cartmsg_.TargetPoints=[0.2,-0.1,0.15,0];
-cartsvc_.call(cartmsg_)
+% due to facing probelms faced during the demo to detect the circle with the camera, we had hard coded this bit to pick and place the circle
+	pickCircle
+   	dropCircle 
+%posDefault;
